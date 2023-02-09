ba5c900099ceb45a8df29cac002041d8d48f9ea3
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-02-09 16:13:12 -0300

    feat: Implementa a função top_5_categories que retorna uma lista com as cinco categorias com maior ocorrência no banco de dados

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,12 @@
+from collections import Counter
+
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    all_categories = [new["category"] for new in news]
+    categories_counter = Counter(sorted(all_categories)).most_common(5)
+
+    return [category[0] for category in categories_counter]
