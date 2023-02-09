daccce122a984083f33b1168e41af90b5737bde6
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-02-09 16:36:34 -0300

    feat: Implementa a função analyzer_menu configurando as mensagens mostradas na tela

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,25 @@
+import sys
+
+
 # Requisitos 11 e 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    option_selected = input(
+        "Selecione uma das opções a seguir:\n "
+        "0 - Popular o banco com notícias;\n "
+        "1 - Buscar notícias por título;\n "
+        "2 - Buscar notícias por data;\n "
+        "3 - Buscar notícias por categoria;\n "
+        "4 - Listar top 5 categorias;\n "
+        "5 - Sair.\n "
+    )
+
+    if option_selected == "0":
+        amount = input("Digite quantas notícias serão buscadas: ")
+    elif option_selected == "1":
+        title = input("Digite o título: ")
+    elif option_selected == "2":
+        date = input("Digite a data no formato aaaa-mm-dd: ")
+    elif option_selected == "3":
+        category = input("Digite a categoria: ")
+    else:
+        return print("Opção inválida", file=sys.stderr)
