fb75b276c5f5c58f70030fcd0a46966a8a74e1b6
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-02-09 16:49:56 -0300

    feat: Implementa a função analyzer_menu funciona, mas tem problema com complexidade

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,5 +1,13 @@
 import sys
 
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_category,
+)
+from tech_news.analyzer.ratings import top_5_categories
+
 
 # Requisitos 11 e 12
 def analyzer_menu():
@@ -15,11 +23,19 @@ def analyzer_menu():
 
     if option_selected == "0":
         amount = input("Digite quantas notícias serão buscadas: ")
+        return get_tech_news(amount)
     elif option_selected == "1":
         title = input("Digite o título: ")
+        return search_by_title(title)
     elif option_selected == "2":
         date = input("Digite a data no formato aaaa-mm-dd: ")
+        return search_by_date(date)
     elif option_selected == "3":
         category = input("Digite a categoria: ")
+        return search_by_category(category)
+    elif option_selected == "4":
+        return top_5_categories()
+    elif option_selected == "5":
+        return print("Encerrando script")
     else:
-        return print("Opção inválida", file=sys.stderr)
+        return sys.stderr.write("Opção inválida\n")
